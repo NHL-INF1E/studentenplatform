1a2f98556062b2dcb3327c7841ca51994dc88813
Stefan <stefan.meijer3@student.nhlstenden.com>
2021-09-30 10:46:46 +0200

    Stockimg, base cards
    Added stock images, with a base card that can be used for loading in data

--- index.php
+++ index.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>websitenaam</title>
-    <link src="css/styles.css" rel="stylesheet">
+    <title>Studentenplatform</title>
+    <link rel="stylesheet" href="css/activiteiten.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
-<body>
-    <?php
-    //Header
-    include_once('');
-    ?>
 
-    <?php
-    //Footer
-    include_once('');
-    ?>
+<body>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-4 kaartje">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img class="img-fluid" src="pictures/stock/hardloopTracks.jpeg" alt="">
+                    </div>
+                    <div class="col-md-12">
+                        <p class="text-start">Titel</p>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="text-start">tekst</p>
+                    </div>
+                    <div class="col-md-12 text-end">
+                        <button class="button"><a href="index.php">Inschrijven</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <p>tekst</p>
+            </div>
+            <div class="col-md-4">
+                <p>tekst</p>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
