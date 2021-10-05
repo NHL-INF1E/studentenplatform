29b808885d2c36d8879c8422e05545d4816c1a5e
RubenVanWoudenberg <ruben.van.woudenberg@student.nhlstenden.com>
2021-10-05 13:01:03 +0200

    header en footer

---
+++ pages/footer.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Footer</title>
+</head>
+<body>
+
+
+    <?php
+        
+    ?>    
+</body>
+</html
---
+++ pages/header.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>header</title>
+    <link rel="stylesheet" rhef="css/headerfooter.css">
+</head>
+<body>
+// NHL Stenden logo
+<div #header>
+    <div>
+    <img src="pictures/NHL_Stenden_Eropuit_Logo.png" 
+         alt="NHL Stenden Eropuit">
+    </div>
+    <div>
+    <a href=activiteiten.php id="button">Activiteiten</a>
+    <a href=login.php id="button">Inloggen</a>
+    <a href=contact.php id="button">Contact</a>
+    </div>
+    <
+    <?php
+
+    ?>
+</body>
+</html>
