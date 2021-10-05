4ba73c891fda6649e5450545c1145830809bdd8f
RubenVanWoudenberg <ruben.van.woudenberg@student.nhlstenden.com>
2021-10-05 13:39:30 +0200

    header en footer aangepast

--- pages/footer.php
+++ pages/footer.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Footer</title>
+    <link href=../css/headerfooter.css rel=stylesheet>
 </head>
 <body>
-
+<!-- footer -->
+    <div id="footer">
+ 
+<!-- Text in de footer -->    
+        <div id="footertext">
+        <p>© NHL Stenden 2021</p>
+        </div>
+        
+    </div> 
 
     <?php
         
--- pages/header.php
+++ pages/header.php
@@ -3,21 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>header</title>
-    <link rel="stylesheet" rhef="css/headerfooter.css">
+    <link href=../css/headerfooter.css rel=stylesheet>
 </head>
 <body>
-// NHL Stenden logo
+<!-- Header -->
 <div #header>
+
+<!-- NHL Stenden Eropuit logo -->
     <div>
-    <img src="pictures/NHL_Stenden_Eropuit_Logo.png" 
-         alt="NHL Stenden Eropuit">
+        <img src="pictures/NHL_Stenden_Eropuit_Logo.png" 
+             alt="NHL Stenden Eropuit">
     </div>
+
+<!-- Header Knoppen naar ander pagina's -->
     <div>
-    <a href=activiteiten.php id="button">Activiteiten</a>
-    <a href=login.php id="button">Inloggen</a>
-    <a href=contact.php id="button">Contact</a>
+        <a href=activiteiten.php id="headerbutton">Activiteiten</a>
+        <a href=login.php id="headerbutton">Inloggen</a>
+        <a href=contact.php id="headerbutton">Contact</a>
     </div>
-    <
+
+<!-- Taal knop -->    
+    <div>
+        
+    </div>
+</div>    
+
     <?php
 
     ?>
