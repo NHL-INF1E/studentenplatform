59fdd5ea8022be12bec8d8fddbde91aface482c0
RubenVanWoudenberg <ruben.van.woudenberg@student.nhlstenden.com>
2021-10-06 11:40:04 +0200

    header update
    header gemaakt, knoppen gemaakt, logo erin gezet. en kleur erin gezet.
    uitlijningen moeten worden aangepast, taal verander knop moet worden gemaakt.

--- pages/header.php
+++ pages/header.php
@@ -12,11 +12,23 @@
     />
   </head>
   <body>
-    <div style="background-color: cornflowerblue;" class="container-fluid">
-
+    <div style="background-color: #005AAA;" class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <img src="../pictures/NHL_Stenden_Eropuit_Logo.png" alt="NHL Stenden Eropuit" id="logoheader">
+          </div>
+          <div class="col-md-5">
+            <p style="line-height:110px">Hallo Stefan</p>
+          </div>
+          <div class="col-md-4" id="buttoncontainerheader">
+            <a href=activiteiten.php id="headerbutton">Activiteiten</a>
+            <a href=login.php id="headerbutton">Inloggen</a>
+            <a href=contact.php id="headerbutton">Contact</a>
+          </div>
+        </div>
     </div>
     <!-- Header
-<div #header>
+<div id="header">
 
 NHL Stenden Eropuit logo
     <div>
