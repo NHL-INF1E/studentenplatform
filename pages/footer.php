dd751da762c1d09aaf6bcdb49564f44555740375
RubenVanWoudenberg <ruben.van.woudenberg@student.nhlstenden.com>
2021-10-06 12:13:31 +0200

    updated footer

--- pages/footer.php
+++ pages/footer.php
@@ -4,20 +4,29 @@
     <meta charset="UTF-8">
     <title>Footer</title>
     <link href=../css/headerfooter.css rel=stylesheet>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF"
+      crossorigin="anonymous"
+    />
 </head>
 <body>
-<!-- footer -->
+<div style="background-color: #005AAA;" class="container-fluid">
+    <div class="col-md-3">
+    <p id="footertext">© NHL Stenden 2021</p>
+    </div>
+    <div class="col-md-9">
+    </div>
+</div>   
+<!-- footer
     <div id="footer">
  
-<!-- Text in de footer -->    
+Text in de footer    
         <div id="footertext">
         <p>© NHL Stenden 2021</p>
         </div>
         
-    </div> 
-
-    <?php
-        
-    ?>    
+    </div> -->  
 </body>
 </html
