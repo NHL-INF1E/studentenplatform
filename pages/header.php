22daa99758810d1c7a7df01f69c7e67e58a9e4b5
RubenVanWoudenberg <ruben.van.woudenberg@student.nhlstenden.com>
2021-10-06 10:15:37 +0200

    header bootstrap

--- pages/header.php
+++ pages/header.php
@@ -1,35 +1,41 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
+  <head>
+    <meta charset="UTF-8" />
     <title>header</title>
     <link href=../css/headerfooter.css rel=stylesheet>
-</head>
-<body>
-<!-- Header -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div style="background-color: cornflowerblue;" class="container-fluid">
+
+    </div>
+    <!-- Header
 <div #header>
 
-<!-- NHL Stenden Eropuit logo -->
+NHL Stenden Eropuit logo
     <div>
         <img src="pictures/NHL_Stenden_Eropuit_Logo.png" 
              alt="NHL Stenden Eropuit">
     </div>
 
-<!-- Header Knoppen naar ander pagina's -->
+Header Knoppen naar ander pagina's
     <div>
         <a href=activiteiten.php id="headerbutton">Activiteiten</a>
         <a href=login.php id="headerbutton">Inloggen</a>
         <a href=contact.php id="headerbutton">Contact</a>
     </div>
 
-<!-- Taal knop -->    
+Taal knop   
     <div>
         
     </div>
 </div>    
-
-    <?php
-
-    ?>
-</body>
+-->
+  </body>
 </html>
